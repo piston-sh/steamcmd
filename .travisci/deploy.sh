53d76bf099818da6d80761c13bbdf672a5f51add
Denis Craig <dcraig@thousandeyes.com>
2018-11-29 21:56:24 +0000

    Only use xenial for now

--- .travisci/deploy.sh
+++ .travisci/deploy.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-# ubuntu-trusty
-docker push sourceforts/steamcmd:ubuntu-trusty-$TRAVIS_BUILD_NUMBER
-docker push sourceforts/steamcmd:ubuntu-trusty
-docker push sourceforts/steamcmd:ubuntu
-docker push sourceforts/steamcmd:latest
-
 # ubuntu-xenial
 docker push sourceforts/steamcmd:ubuntu-xenial-$TRAVIS_BUILD_NUMBER
 docker push sourceforts/steamcmd:ubuntu-xenial
-
-# debian-stretch
-docker push sourceforts/steamcmd:debian-stretch-$TRAVIS_BUILD_NUMBER
-docker push sourceforts/steamcmd:debian-stretch
-docker push sourceforts/steamcmd:debian
+docker push sourceforts/steamcmd:ubuntu
+docker push sourceforts/steamcmd:latest
