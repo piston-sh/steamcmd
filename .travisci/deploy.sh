956bfae92bff4a5bc52f241b8511cf22beed3074
Denis Craig <dcraig@thousandeyes.com>
2018-12-24 00:49:43 +0000

    update docker hub details

--- .travisci/deploy.sh
+++ .travisci/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # ubuntu-xenial
-docker push sourceforts/steamcmd:ubuntu-xenial-$TRAVIS_BUILD_NUMBER
-docker push sourceforts/steamcmd:ubuntu-xenial
-docker push sourceforts/steamcmd:ubuntu
-docker push sourceforts/steamcmd:latest
+docker push pistonsh/steamcmd:ubuntu-xenial-$TRAVIS_BUILD_NUMBER
+docker push pistonsh/steamcmd:ubuntu-xenial
+docker push pistonsh/steamcmd:ubuntu
+docker push pistonsh/steamcmd:latest
